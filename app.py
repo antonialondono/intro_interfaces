e023a600af3ed10ef85be2d69eb16d6dc8347376
antonialondono <alondono2@eafit.edu.co>
2024-08-15 10:25:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,24 @@ st.image(image, caption='Tipos de peces')
 texto=st.text_input('Escribe algo','Este es mi texto')
 st.write('El exto escrito es',texto)
 
+st.subheader("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz",('visual','auditiva','táctil'))
+  if modo == 'visual':
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'auditiva':
+    st.write('La audición es fundamental para tu interfaz')
+  if modo == 'táctil':
+    st.write('El tacto es fundamental para tu interfaz')
 
