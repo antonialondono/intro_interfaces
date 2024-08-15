6f62c8d4506dbf3c928138b5a1e9c164f4520bf7
antonialondono <alondono2@eafit.edu.co>
2024-08-15 10:08:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Creación de interfaces multimodales")
 st.header("Holaaaa")
 st.write("Tipos de letra")
+image = Image.open('peces.jpg')
+
+st.image(image, caption='Tipos de peces')
